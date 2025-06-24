c5a22a0882a90554a420339c997773698a8a4fc3
artemgon <artemhoncharuk123@gmail.com>
2025-06-24 18:17:41 +0300

    Project has been modified.

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
-//1 task
-/*const name = prompt("What's your name?");
+prompt("Task 1");
+const name = prompt("What's your name?");
 alert(`Hi, ${name}`);*/
 
-//2 task
-/*const year = 2025;
+prompt("Task 2");
+const year = 2025;
 birth_year = prompt("Enter your birth year:");
 age = year - birth_year;
-alert(`Your age is ${age}`);*/
+alert(`Your age is ${age}`);
 
-//3 task
-/*square_side = prompt("Enter the length of the side of a square:");
+prompt("Task 3");
+square_side = prompt("Enter the length of the side of a square:");
 perimeter = 4 * square_side;
-alert(`The perimeter of square is ${perimeter}`);*/
+alert(`The perimeter of square is ${perimeter}`);
 
-//4 task
-/*radius = prompt("Enter the radius of circle:");
+prompt("Task 4");
+radius = prompt("Enter the radius of circle:");
 area = Math.PI * radius ** 2;
-alert(`The area of circle is ${area}`);*/
+alert(`The area of circle is ${area}`);
 
-//5 task
-/*distance = prompt("Enter the distance between cities:");
+prompt("Task 5");
+distance = prompt("Enter the distance between cities:");
 time = prompt("Enter the time you want to get to another city:");
 speed = distance / time;
-alert(`Your speed should be ${speed} km/h or higher.`);*/
+alert(`Your speed should be ${speed} km/h or higher.`);
 
-// 6 task
-/*const euro = 0.86;
+prompt("Task 6");
+const euro = 0.86;
 dollars = prompt("Enter the quantity of dollars:");
 result = dollars * euro;
-alert(`The amount is ${result}`);*/
+alert(`The amount is ${result}`);
 
-//7 task
-/*storage_capacity = prompt("Enter storage capacity");
+prompt("Task 7");
+storage_capacity = prompt("Enter storage capacity");
 quantity = Math.floor(storage_capacity * 1024 / 820);
-alert(`You can download file ${quantity} times`);*/
+alert(`You can download file ${quantity} times`);
 
-//8 task
-/*money = prompt("Enter an amount of money you have:");
+prompt("Task 8");
+money = prompt("Enter an amount of money you have:");
 chocolate_price = prompt("Enter a price of one chocolate bar:");
 quantity_1 = money / chocolate_price;
 quantity_2 = Math.floor(quantity_1);
 rest = quantity_1 - quantity_2;
 alert(`You can buy ${quantity_2} chocolate bars`);
-alert(`Your rest is ${rest}`);*/
+alert(`Your rest is ${rest}`);
 
-//9 task
-/*result = 0;
+prompt("Task 9");
+result = 0;
 number = prompt("Enter 3-digit number:");
 result += number % 10 * 100;
 number -= number % 10;
@@ -54,8 +54,8 @@ number -= number % 100;
 result += number / 100;
 alert(`Palindrome of your number is ${result}.`);*/
 
-//10 task
-/*integer = prompt("Enter an integer:");
+prompt("Task 10");
+integer = prompt("Enter an integer:");
 isEven = integer % 2 === 0;
 result = (isEven && "even") || "odd";
-alert(`Your number is ${result}.`);*/
+alert(`Your number is ${result}.`);
