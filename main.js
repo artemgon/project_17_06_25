ec1f567c5d6f37f43ce8ccd6e9f9c6a7db96a54a
artemgon <artemhoncharuk123@gmail.com>
2025-06-24 15:17:51 +0300

    Project has been created.

---
+++ main.js
@@ -0,0 +1,61 @@
+//1 task
+/*const name = prompt("What's your name?");
+alert(`Hi, ${name}`);*/
+
+//2 task
+/*const year = 2025;
+birth_year = prompt("Enter your birth year:");
+age = year - birth_year;
+alert(`Your age is ${age}`);*/
+
+//3 task
+/*square_side = prompt("Enter the length of the side of a square:");
+perimeter = 4 * square_side;
+alert(`The perimeter of square is ${perimeter}`);*/
+
+//4 task
+/*radius = prompt("Enter the radius of circle:");
+area = Math.PI * radius ** 2;
+alert(`The area of circle is ${area}`);*/
+
+//5 task
+/*distance = prompt("Enter the distance between cities:");
+time = prompt("Enter the time you want to get to another city:");
+speed = distance / time;
+alert(`Your speed should be ${speed} km/h or higher.`);*/
+
+// 6 task
+/*const euro = 0.86;
+dollars = prompt("Enter the quantity of dollars:");
+result = dollars * euro;
+alert(`The amount is ${result}`);*/
+
+//7 task
+/*storage_capacity = prompt("Enter storage capacity");
+quantity = Math.floor(storage_capacity * 1024 / 820);
+alert(`You can download file ${quantity} times`);*/
+
+//8 task
+/*money = prompt("Enter an amount of money you have:");
+chocolate_price = prompt("Enter a price of one chocolate bar:");
+quantity_1 = money / chocolate_price;
+quantity_2 = Math.floor(quantity_1);
+rest = quantity_1 - quantity_2;
+alert(`You can buy ${quantity_2} chocolate bars`);
+alert(`Your rest is ${rest}`);*/
+
+//9 task
+/*result = 0;
+number = prompt("Enter 3-digit number:");
+result += number % 10 * 100;
+number -= number % 10;
+result += number % 100;
+number -= number % 100;
+result += number / 100;
+alert(`Palindrome of your number is ${result}.`);*/
+
+//10 task
+/*integer = prompt("Enter an integer:");
+isEven = integer % 2 === 0;
+result = (isEven && "even") || "odd";
+alert(`Your number is ${result}.`);*/
